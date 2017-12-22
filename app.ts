6404a99f29b422dbd0af8441faccb2ae7f533f1e
Elliot Huffman <thelich2@gmail.com>
2017-12-22 15:35:00 -0500

    Add MongoDB + DevDependencies
    Add a proof of concept for MongoDB.
    Refactor variables.
    Rebuild annomous functions to be arrow functions.
    Add dev dependencies.

--- app.ts
+++ app.ts
@@ -1,40 +1,89 @@
 import * as restify from "restify";
 import * as mongoDB from "mongodb";
 
-const server = restify.createServer({
+const APIServer = restify.createServer({
   name: 'Three Horsemen API Server',
   version: '1.0.0'
 });
-const serverSettings = {
-  port: 8080,
-  listenOn: "localhost"
+
+const ServerConfig = {
+  APIPort: 8080,
+  APIListenOn: "localhost",
+  DBurl: "mongodb://localhost:27017",
+  DBName: "API"
 }
 
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
-server.use(
-    function crossOrigin(req,res,next){
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      return next();
-    }
-  );
+// MongoDB Write
+const insertDocuments = (db, callback) => {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], (error, result) => {
+    if (error) throw error;
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
 
-server.get('/', function (req, res, next) {
+
+// MongoDB Read
+const findDocuments = (db, callback) => {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray((error, docs) => {
+    if (error) throw error;
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+
+// MongoDB Main
+mongoDB.connect(ServerConfig.DBurl, (error, clientInstance) => {
+  if (error) throw error;
+  console.log("Connected successfully to server");
+
+  const db = clientInstance.db(ServerConfig.DBName);
+
+  insertDocuments(db, () => {clientInstance.close()});
+  findDocuments(db, () => {clientInstance.close()});
+});
+
+
+// Set up server plugins and allow CORS
+APIServer.use(restify.plugins.acceptParser(APIServer.acceptable));
+APIServer.use(restify.plugins.queryParser());
+APIServer.use(restify.plugins.bodyParser());
+APIServer.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
+
+// Create a new get URL on the server
+APIServer.get('/', (req, res, next) => {
   console.log(next);
   res.send({ hello: 'world' });
   return next();
 });
 
-server.get('/activities/:categories', function (req, res, next) {
+// Create a new get URL on the server
+APIServer.get('/activities/:categories', (req, res, next) => {
   res.send(req.params);
   return next();
 });
 
-server.get('/activities/:categories/:activity', function (req, res, next) {
+// Create a new get URL on the server
+APIServer.get('/activities/:categories/:activity', (req, res, next) => {
   res.send(req.params);
   return next();
 });
 
-server.listen(serverSettings.port, serverSettings.listenOn);
+// Start API APIServer.
+APIServer.listen(ServerConfig.APIPort, ServerConfig.APIListenOn);
