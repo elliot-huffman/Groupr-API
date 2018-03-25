fdbcc0d7525a05cac6c06a449c5c4d6f10d7c139
Elliot Huffman <ehuffman@elliot-labs.com>
2018-03-25 14:00:11 -0400

    Import database script
    Import the entire database script.
    Modify the contents of the queuer script as a result.

--- src/Queuer.ts
+++ src/Queuer.ts
@@ -1,16 +1,19 @@
-import { ObjectID as ObjectIDType } from "./Database";
+import {QueuerConfig} from "./Config";
+import * as MongoInterface from "./Database";
+
+const Database = new MongoInterface.Database(QueuerConfig.Database.Host, QueuerConfig.Database.DatabaseName, QueuerConfig.Database.Port);
 
 // Set the user onto a category.
-function SelectCategory(CategoryID: ObjectIDType) {
+function SelectCategory(CategoryID: MongoInterface.ObjectID) {
     // pass
 }
 
 // After a user joins a category, sort them into a queue.
-function SortUser(UserID: ObjectIDType) {
+function SortUser(UserID: MongoInterface.ObjectID) {
     // pass
 }
 
 // After a queue fills up, send the message or set the results to the user profile.
-function FulfillQueue(QueueID: ObjectIDType) {
+function FulfillQueue(QueueID: MongoInterface.ObjectID) {
     // pass
 }
