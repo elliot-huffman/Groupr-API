8ba9d1f069b8796967572d5cb08bf6dde128e614
Elliot Huffman <ehuffman@elliot-labs.com>
2018-04-05 17:32:24 -0400

    Initial weighted random functions
    These are just being saved for future refrence. These are no way in shape for use in a finished product.
    
    Reworked some types for place holders.

--- src/Queuer.ts
+++ src/Queuer.ts
@@ -4,12 +4,12 @@ import * as MongoInterface from "./Database";
 const Database = new MongoInterface.Database(QueuerConfig.Database.Host, QueuerConfig.Database.DatabaseName, QueuerConfig.Database.Port);
 
 // Set the user onto a category.
-function SelectCategory(CategoryID: MongoInterface.ObjectID) {
+function SelectCategory(UserID: MongoInterface.ObjectID, CategoryID: MongoInterface.ObjectID) {
     // pass
 }
 
 // After a user joins a category, sort them into a queue.
-function SortUser(UserID: MongoInterface.ObjectID) {
+function SortUser(CategoryID: MongoInterface.ObjectID) {
     // pass
 }
 
@@ -17,3 +17,31 @@ function SortUser(UserID: MongoInterface.ObjectID) {
 function FulfillQueue(QueueID: MongoInterface.ObjectID) {
     // pass
 }
+
+// Dump truck
+function weightedRand(spec) {
+    var i, j, table=[];
+    for (i in spec) {
+        // The constant 10 below should be computed based on the
+        // weights in the spec for a correct and optimal table size.
+        // E.g. the spec {0:0.999, 1:0.001} will break this impl.
+        for (j=0; j<spec[i]*10; j++) {
+        table.push(i);
+        }
+    }
+    return function() {
+        return table[Math.floor(Math.random() * table.length)];
+    }
+}
+var rand012 = weightedRand({0:0.8, 1:0.1, 2:0.1});
+rand012(); // random in distribution...
+
+// race car
+function weightedRand2(spec) {
+    var i, sum=0, r=Math.random();
+    for (i in spec) {
+        sum += spec[i];
+        if (r <= sum) return i;
+    }
+}
+weightedRand2({0:0.8, 1:0.1, 2:0.1}); // random in distribution...
